dbc7168d7e15e5b2cc663a6a9324656f821784fa
luyibiao <1739717921@qq,com>
2021-02-23 17:10:48 +0800

    提交

---
+++ src/components/select/main.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="select-main">
+    <input 
+    type="text" 
+    class="select-main_input"
+    @focus.self="focus" 
+    @click.stop="() => {}"
+   
+    readonly 
+    :value="value"/>
+    <div class="select-main_wrap" v-show="show">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: [String, Number, Boolean],
+  },
+  provide () {
+    return {
+      options: this,
+    }
+  },
+  watch: {
+    show(val) {
+      if (val) {
+        setTimeout(() => {
+          window.addEventListener('click', this.callback, false)
+        }, 1000)
+      } else {
+        window.removeEventListener('click', this.callback)
+      }
+    }
+  },
+  data() {
+    return {
+      show:false
+    }
+  },
+  methods: {
+    callback() {
+      this.show = false
+    },
+    focus() {
+      this.show = true
+    },
+    getData (options) {
+      this.$emit('input', options.value)
+      this.show = false
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+  .select-main {
+    position: relative;
+    display: inline-block;
+    .select-main_input {
+      cursor: pointer;
+      height: 20px;
+    }
+    .select-main_wrap {
+      position: absolute;
+      background: #fff;
+      border: 1px solid #000;
+      left: 0;
+      right: 0;
+      top: 30px;
+    }
+  }
+</style>
---
+++ src/components/select/mian-item.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="select-main_options" @click.stop="onSelect()">
+    <slot>{{label}}</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: [String, Boolean, Number],
+      default: ''
+    },
+    value: {
+      type: [String, Boolean, Number],
+      default: ''
+    },
+  },
+  inject: ['options'],
+  methods: {
+    onSelect () {
+      this.options.getData(this)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .select-main_options {
+    // background: ;
+    padding: 10px;
+    box-sizing: border-box;
+    cursor: pointer;
+    &:hover {
+      background: #eee;
+    }
+  }
+  .select-main_options + .select-main_options {
+    border-top: 1px solid #eee;
+  }
+</style>
